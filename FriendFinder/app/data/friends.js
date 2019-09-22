1ad0cd59c7569b43b806e504303a718812187bd7
CapraRoyale <siege.lehman@gmail.com>
2019-09-21 21:28:39 -0700

    Created all necessary resources, initial coding necessary for core functionality.

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,117 @@
+var friends =
+    [{
+        name: "Alpha",
+        photo: "https://commons.wikimedia.org/wiki/Category:Alpha_(letter)#/media/File:ALPHA_(capital_and_small).png",
+        scores: [
+            1,
+            2,
+            3,
+            4,
+            5,
+            1,
+            2,
+            3,
+            4,
+            5
+        ]
+    },
+    {
+        name: "Beta",
+        photo: "https://commons.wikimedia.org/wiki/Category:Beta_(letter)#/media/File:BETA_(capital_and_small).png",
+        scores: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    },
+    {
+        name: "Gamma",
+        photo: "https://commons.wikimedia.org/wiki/Category:Gamma_(letter)#/media/File:GAMMA_(capital_and_small).png",
+        scores: [
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2
+        ]
+    },
+    {
+        name: "Delta",
+        photo: "https://commons.wikimedia.org/wiki/Category:Delta_(letter)#/media/File:DELTA_(capital_and_small).png",
+        scores: [
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+        ]
+    },
+    {
+        name: "Epsilon",
+        photo: "https://commons.wikimedia.org/wiki/Category:Epsilon_(letter)#/media/File:EPSILON_(capital_and_small).png",
+        scores: [
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4
+        ]
+    },
+    {
+        name: "Zeta",
+        photo: "https://commons.wikimedia.org/wiki/Category:Zeta_(letter)#/media/File:ZETA_(capital_and_small).png",
+        scores: [
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+        ]
+    },
+    {
+        name: "Eta",
+        photo: "https://commons.wikimedia.org/wiki/Category:Eta_(letter)#/media/File:ETA_(capital_and_small).png",
+        scores: [
+            2,
+            3,
+            2,
+            2,
+            3,
+            2,
+            3,
+            3,
+            2,
+            3
+        ]
+    }
+
+    ];
+
+module.exports = friends;
---
+++ FriendFinder/app/routing/apiRoutes.js
@@ -0,0 +1,36 @@
+var friends = require("../data/friends.js")
+
+module.exports = function (app) {
+
+    app.get("/api/friends", function (req, res) {
+        return res.json(friends);
+    });
+
+
+    app.post("/api/friends", function (req, res) {
+  
+        var friendScores = req.body.scores;
+        var scores = [];
+
+        // looping over friends array
+        for (var i = 0; i < friends.length; i++) {
+            var minScoresDifference = 0;
+            var maxScoresDifference = 40;
+            // looping through new friends scores and adding to the min score difference which is already 0. Adding the scores from within the individual friends minus the new friends scores.
+            for (var x = 0; x < friendScores.length; x++) {
+                minScoresDifference += (Math.abs(parseInt(friends[i].scores[x]) - parseInt(friendScores[x])));
+            }
+            scores.push(minScoresDifference)
+        }
+        var indexOfSmallestValue = indexOfSmallest(scores)
+        var bestFriend = friends[indexOfSmallestValue]
+        friends.push(req.body);
+        res.json(bestFriend);
+
+    });
+
+
+
+}
+// function to find the index of the smallest element from an array
+function indexOfSmallest(a) { return a.indexOf(Math.min.apply(Math, a)); }
